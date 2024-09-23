d0d51fa08458c38092ebfcc001d796cc767fabf9
Jiayang Lai <insiderdavidlai@gmail.com>
2024-09-22 22:18:50 -0400

    Fixed a bug existed in last commit that prevent store generatedBy into database, caused by ShortenServices directly storing info into database and ignoring URLService
    Implemented deletion, getlist method for URLs similiar to QRcode list
    
    -Next Planned Implementation:
    1.After user clicked url in URLs list, it will be redirected.
    2. Oauth
    3. Redis

--- src/main/java/com/example/demo/service/Base62ShortenService.java
+++ src/main/java/com/example/demo/service/Base62ShortenService.java
@@ -1,45 +1,29 @@
 package com.example.demo.service;
 
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import static com.mongodb.client.model.Filters.eq;
 
 @Service
 public class Base62ShortenService implements ShortenService {
 
-    private final MongoCollection<Document> collection;
+    private URLService urlService;
 
     @Autowired
-    public Base62ShortenService(MongoDatabase mongoDatabase) {
-        this.collection = mongoDatabase.getCollection("URLMapping");
+    public void setUrlService(URLService urlService) {
+        this.urlService = urlService;
     }
 
     @Override
     public String shortenURL(String longURL) {
         // Check if the longURL already exists in the database
-        Document existingDoc = collection.find(eq("longURL", longURL)).first();
-        if (existingDoc != null) {
+        if (urlService.existsByLongURL(longURL)) {
             // Return the existing shortURL if the longURL already exists
-            return existingDoc.getString("shortURL");
+            return urlService.findByLongURL(longURL).getShortURL();
         }
 
         // Generate a new shortURL if the longURL does not exist
         String shortURL = "http://tiny.url/" + encode(System.currentTimeMillis());
-        Document doc = new Document("longURL", longURL)
-                .append("shortURL", shortURL)
-                .append("type", "base62to10");
-        collection.insertOne(doc);
         return shortURL;
-    }
-
-    @Override
-    public String getLongURL(String shortURL) {
-        Document doc = collection.find(eq("shortURL", shortURL)).first();
-        return doc != null ? doc.getString("longURL") : null;
     }
 
     public String encode(long num) {
